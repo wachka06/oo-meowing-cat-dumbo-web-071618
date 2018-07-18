83a0a51b309a1f8268b50ce25cfdfc19e297730d
wachka06 <wachka06@gmail.com>
2018-07-18 19:33:42 +0000

    done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,29 @@
 ## code your solution here. 
+
+# class Cat 
+#   attr_accessor :name, :age
+  
+  
+#   def initialize(name, num)
+#     @name = name
+#     @age = num
+#   end
+  
+#   def meow
+#     puts "meow!"
+#   end
+  
+# end
+
+# cat = Cat.new("Maru", 16)
+
+# puts cat.age
+
+
+class Cat 
+  attr_accessor:name, :age 
+  
+  def meow
+    puts "meow!"
+  end
+end
